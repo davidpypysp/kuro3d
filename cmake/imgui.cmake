4c270b9565ce5e7624dbc1ee981047e6da7d31df
dv <davidpy1992@gamil.com>
2021-02-11 21:13:03 -0800

    demo work

--- cmake/imgui.cmake
+++ cmake/imgui.cmake
@@ -4,30 +4,23 @@ file(GLOB IMGUI_SOURCES
     ${IMGUI_INCLUDE_DIR}/backends/imgui_impl_opengl3.cpp
     ${IMGUI_INCLUDE_DIR}/backends/imgui_impl_glfw.cpp
 )
-file(GLOB IMGUI_HEADERS
-    ${IMGUI_INCLUDE_DIR}/*.h
-    ${IMGUI_INCLUDE_DIR}/backends/imgui_impl_opengl3.h
-    ${IMGUI_INCLUDE_DIR}/backends/imgui_impl_glfw.h
-)
-
-message("cpps" ${IMGUI_SOURCES})
-
 add_library(imgui STATIC ${IMGUI_SOURCES})
 
 add_definitions(-DIMGUI_IMPL_OPENGL_LOADER_GLAD)
 
 include_directories(
     ${IMGUI_INCLUDE_DIR}
+    ${IMGUI_INCLUDE_DIR}/backends
     ${OPENGL_INCLUDE_DIR}
     ${GLFW_INCLUDE_DIR}
     ${GLAD_INCLUDE_DIR}
-    )
+)
 
 target_link_libraries(imgui
     ${OPENGL_LIBRARIES}
     ${GLFW_LIBRARIES}
     ${GLAD_LIBRARIES}
-    )
+)
 
 set_target_properties(imgui PROPERTIES LINKER_LANGUAGE CXX)
 set_target_properties(imgui PROPERTIES FOLDER third_party)
