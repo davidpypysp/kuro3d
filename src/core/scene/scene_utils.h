041e3e9e0ff6f79715ec0d6527c23a1e63925d7f
dv <davidpy1992@gmail.com>
2021-12-28 11:20:22 -0800

    refa

--- src/core/scene/scene_utils.h
+++
@@ -1,8 +0,0 @@
-#ifndef SRC_CORE_SCENE_SCENE_UTILS_H_
-#define SRC_CORE_SCENE_SCENE_UTILS_H_
-
-#include "src/core/scene/scene_node.h"
-
-namespace kuro {}
-
-#endif  // SRC_CORE_SCENE_SCENE_UTILS_H_
