2b5c8b5cc16743347d1234292db5d1ccfdd8e563
dv <davidpy1992@gmail.com>
2022-01-17 11:11:24 -0800

    refactor

--- src/core/scene/scene_controller.cc
+++
@@ -1,9 +0,0 @@
-#include "src/core/scene/scene_controller.h"
-
-#include "src/core/scene/scene_manager.h"
-
-namespace kuro {
-
-SceneController::SceneController() { Register<SceneManager>(); }
-
-}  // namespace kuro
